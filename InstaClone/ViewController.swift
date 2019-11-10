fcda8faf35827ab5a07b15650c3f10b9a666a7b8
bryanr1992 <bryan.rivas05@gmail.com>
2019-11-09 19:24:38 -0500

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Bryan Rivas on 11/9/19.
+//  Copyright © 2019 Bryan Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
